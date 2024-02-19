79d0aefa6b3514b7364d9e3a6aaae585bb2629a2
kangjian.wu <kangjian.wu@xuanyutech.net>
2024-02-19 18:06:45 +0800

    net/http

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,11 +10,14 @@ func main() {
 	// 创建一个新的 Gin 引擎
 	r := gin.Default()
 
-	// 添加一个路由，当有人访问根路径时，会返回 "Hello, World!"
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.String(http.StatusOK, "Hello, looksword!")
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusNotFound, "404 Not Found")
 	})
 
 	// 运行 Gin 引擎
-	r.Run()
+	r.Run(":9393")
 }
